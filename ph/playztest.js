b4d49fbdc7e36c1cabd3cc971b0526f031d52c4b
connectdigital <ulas@connect.digital>
2016-02-29 16:53:50 +0200

    Create playztest.js

---
+++ ph/playztest.js
@@ -0,0 +1,31 @@
+
+   document.write('<div id="-stick-footer-1455632411732" style="  border-top-width: 1px; border-top-style: solid; border-top-color: grey; position: fixed; left: 0px; width: 100%; height: auto; z-index: 346; opacity: 1; bottom: -1px; background: rgba(240, 240, 240, 0.498039);"><a onclick=\'document.body.removeChild( document.getElementById("-stick-footer-1455632411732") )\'  href="#" style="position: absolute; top:-3px; width: 27px; height: 27px; left: 50%; margin-left: -366px;z-index: 999999999;"><img alt="close" src="http://cdn.connect.digital/close.png" style="border: none"></a><div style="padding-top:0px; text-align: center; width: 100%; height:auto; margin: 0px; ">');
+
+  
+  var googletag = googletag || {};
+  googletag.cmd = googletag.cmd || [];
+  (function() {
+    var gads = document.createElement('script');
+    gads.async = true;
+    gads.type = 'text/javascript';
+    var useSSL = 'https:' == document.location.protocol;
+    gads.src = (useSSL ? 'https:' : 'http:') +
+      '//www.googletagservices.com/tag/js/gpt.js';
+    var node = document.getElementsByTagName('script')[0];
+    node.parentNode.insertBefore(gads, node);
+  })();
+
+
+
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/23585023/Playz_Criteo_Test', [728, "auto"], 'div-gpt-ad-1456757425218-0').addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
+
+
+document.write("<div id='div-gpt-ad-1456757425218-0' style='height:auto; width:728px; position: relative;left: 50%;margin-left: -364px;'><scri"+"pt type='text/javascript'>googletag.cmd.push(function() { googletag.display('div-gpt-ad-1456757425218-0'); });</scri"+"pt></div>");
+
+document.write('<a href="http://connect.digital" style="position: absolute;top: 65px;right: 10px;"><img src="http://cdn.connect.digital/logo.png"></a>');
+
+document.write('</div></div>');
