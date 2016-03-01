a07a6d64b3c514b546145b6cc75399745bb40a12
connectdigital <ulas@connect.digital>
2016-03-01 13:01:04 +0200

    Update playz.js

--- ph/playz.js
+++ ph/playz.js
@@ -1,5 +1,5 @@
 
-
+document.write('<div><div>');
 
   googletag.cmd.push(function() {
     googletag.defineSlot('/23585023/Playz.com//Desktop//Crt//728x90', [728, 90], 'div-gpt-ad-1456780504093-0').addService(googletag.pubads());
@@ -9,3 +9,5 @@
 
 
 document.write("<div id='div-gpt-ad-1456780504093-0' style='height:90px; width:728px;'><scr"+"ipt type='text/javascript'>googletag.cmd.push(function() { googletag.display('div-gpt-ad-1456780504093-0'); });</scr"+"ipt></div>")
+
+document.write('</div></div>');
