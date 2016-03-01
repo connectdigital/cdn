def9e2e0d2de34863c26b1cfe3aa8a5d5ed173ba
connectdigital <ulas@connect.digital>
2016-03-01 15:41:40 +0200

    Update video-rengi.js

--- ph/video-rengi.js
+++ ph/video-rengi.js
@@ -3,7 +3,7 @@
 (function(){
   
 var position_add = "<scr"+"ipt type='text/javascript'> googletag.cmd.push(function() {googletag.defineSlot('/23585023/video-rengi.net//Desktop//Crt//728x90', [728, 90], 'div-gpt-ad-1456837493050-0').addService(googletag.pubads());googletag.pubads().enableSingleRequest();googletag.pubads().collapseEmptyDivs();googletag.enableServices();});</scr"+"ipt>";
-var body_code = "<div id='div-gpt-ad-1456837493050-0' style='height:90px; width:728px;'><scr"+"ipt type='text/javascript'>googletag.cmd.push(function() { googletag.display('div-gpt-ad-1456837493050-0'); });</scr"+"ipt></div>";
+var body_code = "<div id='div-gpt-ad-1456837493050-0' style='height:90px; width:728px;position: relative;left: 50%;margin-left: -364px;'><scr"+"ipt type='text/javascript'>googletag.cmd.push(function() { googletag.display('div-gpt-ad-1456837493050-0'); });</scr"+"ipt></div>";
 
 
 
