2c8ab1682d073c38341ce60bc0cd0029dd248181
connectdigital <ulas@connect.digital>
2016-03-01 15:45:19 +0200

    Update video-rengi.js

--- ph/video-rengi.js
+++ ph/video-rengi.js
@@ -5,7 +5,7 @@
 var position_add = "<scr"+"ipt type='text/javascript'> googletag.cmd.push(function() {googletag.defineSlot('/23585023/video-rengi.net//Desktop//Crt//728x90', [728, 90], 'div-gpt-ad-1456837493050-0').addService(googletag.pubads());googletag.pubads().enableSingleRequest();googletag.pubads().collapseEmptyDivs();googletag.enableServices();});</scr"+"ipt>";
 var body_code = "<div id='div-gpt-ad-1456837493050-0' style='height:90px; width:728px;position: relative;left: 50%;margin-left: -364px;'><scr"+"ipt type='text/javascript'>googletag.cmd.push(function() { googletag.display('div-gpt-ad-1456837493050-0'); });</scr"+"ipt></div>";
 
-
+body_code = "";
 
 // head-sabit
 document.write("<scr"+"ipt type='text/javascript'> var googletag = googletag || {};googletag.cmd = googletag.cmd || [];(function() {var gads = document.createElement('script');gads.async = true;gads.type = 'text/javascript';var useSSL = 'https:' == document.location.protocol;gads.src = (useSSL ? 'https:' : 'http:') +'//www.googletagservices.com/tag/js/gpt.js';var node = document.getElementsByTagName('script')[0];node.parentNode.insertBefore(gads, node);})(); </scr"+"ipt>   ");
