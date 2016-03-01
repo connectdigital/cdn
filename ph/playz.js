c178962852693e217681ed95677cc58f26522da1
connectdigital <ulas@connect.digital>
2016-03-01 13:19:22 +0200

    Update playz.js

--- ph/playz.js
+++ ph/playz.js
@@ -1,18 +1,11 @@
+var position_add = "<scr"+"ipt type='text/javascript'> googletag.cmd.push(function() {googletag.defineSlot('/23585023/Playz.com//Desktop//Crt//728x90', [728, 90], 'div-gpt-ad-1456780504093-0').addService(googletag.pubads());googletag.pubads().enableSingleRequest();googletag.enableServices();}); </scr"+"ipt>";
+var body_code = "<div id='div-gpt-ad-1456780504093-0' style='height:90px; width:728px;'><scr"+"ipt type='text/javascript'>googletag.cmd.push(function() { googletag.display('div-gpt-ad-1456780504093-0'); });</scr"+"ipt></div>";
+
+
+// head-sabit
 document.write("<scr"+"ipt type='text/javascript'> var googletag = googletag || {};googletag.cmd = googletag.cmd || [];(function() {var gads = document.createElement('script');gads.async = true;gads.type = 'text/javascript';var useSSL = 'https:' == document.location.protocol;gads.src = (useSSL ? 'https:' : 'http:') +'//www.googletagservices.com/tag/js/gpt.js';var node = document.getElementsByTagName('script')[0];node.parentNode.insertBefore(gads, node);})(); </scr"+"ipt>   ");
-document.write("<scr"+"ipt type='text/javascript'> googletag.cmd.push(function() {googletag.defineSlot('/23585023/Playz.com//Desktop//Crt//728x90', [728, 90], 'div-gpt-ad-1456780504093-0').addService(googletag.pubads());googletag.pubads().enableSingleRequest();googletag.enableServices();}); </scr"+"ipt>");
-
-
-
-
-
+document.write(position_add);
 document.write('<div id="-stick-footer-1455632411732" style="  border-top-width: 1px; border-top-style: solid; border-top-color: grey; position: fixed; left: 0px; width: 100%; height: auto; z-index: 346; opacity: 1; bottom: -1px; background: rgba(240, 240, 240, 0.498039);"><a onclick=\'document.body.removeChild( document.getElementById("-stick-footer-1455632411732") )\'  href="#" style="position: absolute; top:-3px; width: 27px; height: 27px; left: 50%; margin-left: 400px;z-index: 999999999;"><img alt="close" src="http://cdn.connect.digital/close.png" style="border: none"></a><div style="padding-top:0px; text-align: center; width: 100%; height:auto; margin: 0px; ">');
-
-// body
-document.write("<div id='div-gpt-ad-1456780504093-0' style='height:90px; width:728px;'><scr"+"ipt type='text/javascript'>googletag.cmd.push(function() { googletag.display('div-gpt-ad-1456780504093-0'); });</scr"+"ipt></div>")
-
-
+document.write(body_code)
 document.write('<a href="http://connect.digital" style="position: absolute;top: 65px;right: 10px;"><img src="http://cdn.connect.digital/logo.png"></a>');
 document.write('</div></div>');
-
-
-console.log("v-3")
