138cff9d0c4fcb520387894427d4d523b3ed36e2
connectdigital <ulas@connect.digital>
2016-03-02 15:53:22 +0200

    Update playz.js

--- ph/playz.js
+++ ph/playz.js
@@ -2,7 +2,7 @@
   
 var position_add = "<scr"+"ipt type='text/javascript'> googletag.cmd.push(function() {googletag.defineSlot('/23585023/Playz.com//Desktop//Crt//728x90', [728,90], 'div-gpt-ad-1456780504093-0').addService(googletag.pubads());googletag.pubads().enableSingleRequest();googletag.enableServices();}); </scr"+"ipt>";
 
-var body_code = "<div id='div-gpt-ad-1456780504093-0' style='height:90px; width:728px;'><scr"+"ipt type='text/javascript'>googletag.cmd.push(function() { googletag.display('div-gpt-ad-1456780504093-0'); });</scr"+"ipt></div>";
+var body_code = "<div id='div-gpt-ad-1456780504093-0' style='height:90px; width:728px;position: relative;left: 50%;margin-left: -364px;'><scr"+"ipt type='text/javascript'>googletag.cmd.push(function() { googletag.display('div-gpt-ad-1456780504093-0'); });</scr"+"ipt></div>";
 
 
         
