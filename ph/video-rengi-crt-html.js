8267511c367fb42632031e233ee6331822ce22c8
connectdigital <ulas@connect.digital>
2016-03-03 18:01:52 +0200

    Update video-rengi-crt-html.js

--- ph/video-rengi-crt-html.js
+++ ph/video-rengi-crt-html.js
@@ -1,10 +1,10 @@
 (function(){
   console.log("1")
-document.write('<div id="-stick-footer-1455632411732" style="  border-top-width: 1px; border-top-style: solid; border-top-color: grey; position: fixed; left: 0px; width: 100%; height: auto; z-index: 346; opacity: 1; bottom: -1px; background: rgba(240, 240, 240, 0.498039);"><a onclick=\'document.body.removeChild( document.getElementById("-stick-footer-1455632411732") )\'  href="#" style="position: absolute;top: 0px;width: 27px;height: 27px;right: 20px;z-index: 999999999;"><img alt="close" src="http://cdn.connect.digital/close.png" style="border: none"></a><div style="padding-top:0px; text-align: center; width: 100%; height:auto; margin: 0px; ">');
+document.write('<div id="-stick-footer-1455632411732" style="  border-top-width: 1px; border-top-style: solid; border-top-color: grey; position: fixed; left: 0px; width: 100%; height: auto; z-index: 346; opacity: 1; bottom: -1px; background: rgba(240, 240, 240, 0.498039);"><a onclick=\'document.body.removeChild( document.getElementById("-stick-footer-1455632411732") )\'  href="#" style="position: absolute;top: -3px;width: 27px;height: 27px;left: 50%;margin-left: 330px;z-index: 999999999;"><img alt="close" src="http://cdn.connect.digital/close.png" style="border: none"></a><div style="padding-top:0px; text-align: center; width: 100%; height:auto; margin: 0px; ">');
 
-document.write('<iframe src="http://cdn.connect.digital/ph/video-rengi-crt.html" style="width: 728px;height: auto;border: none;"></iframe>');
+document.write('<iframe src="http://cdn.connect.digital/ph/video-rengi-crt.html" style="width: 728px;height: 90px;border: none;"></iframe>');
 
-document.write('<a href="http://connect.digital" style="position: absolute;top: 65px;right: 10px;"><img src="http://cdn.connect.digital/logo.png"></a>');
+// document.write('<a href="http://connect.digital" style="position: absolute;top: 65px;right: 10px;"><img src="http://cdn.connect.digital/logo.png"></a>');
 document.write('</div></div>');
 
 })();
