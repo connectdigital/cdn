23c3cafdaa3cb82a9f5075615f2427c2d13d543a
connectdigital <ulas@connect.digital>
2016-03-03 18:41:26 +0200

    Update zeptex-crt-728x90.js

--- ph/zeptex-crt-728x90.js
+++ ph/zeptex-crt-728x90.js
@@ -1,5 +1,8 @@
-<script type='text/javascript'>
-<!--//<![CDATA[
+(function(){
+  console.log("1")
+document.write('<div id="-stick-footer-1455632411732" style="  border-top-width: 1px; border-top-style: solid; border-top-color: grey; position: fixed; left: 0px; width: 100%; height: auto; z-index: 346; opacity: 1; bottom: -1px; background: rgba(240, 240, 240, 0.498039);"><a onclick=\'document.body.removeChild( document.getElementById("-stick-footer-1455632411732") )\'  href="#" style="position: absolute;top: 0px;width: 27px;height: 27px;right: 20px;z-index: 999999999;"><img alt="close" src="http://cdn.connect.digital/close.png" style="border: none"></a><div style="padding-top:0px; text-align: center; width: 100%; height:auto; margin: 0px; ">');
+
+
    document.MAX_ct0 ='';
    var m3_u = (location.protocol=='https:'?'https://cas.criteo.com/delivery/ajs.php?':'http://cas.criteo.com/delivery/ajs.php?');
    var m3_r = Math.floor(Math.random()*99999999999);
@@ -18,4 +21,9 @@
    var macro = "%%SI" + "TE%%";
    if (publisherurl !== macro) document.write ("&amp;publisherurl="+publisherurl);
    document.write ("'></scr"+"ipt>");
-//]]>--></script>
+
+
+document.write('<a href="http://connect.digital" style="position: absolute;top: 65px;right: 10px;"><img src="http://cdn.connect.digital/logo.png"></a>');
+document.write('</div></div>');
+
+})();
